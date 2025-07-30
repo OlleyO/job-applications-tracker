492832db9efc09091ca4d159cbf750d936864efa
Oleh Selivanov <my.mail.olech@gmail.com>
2025-07-30 16:38:02 +0200

    feat: add components, create navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import Link from "next/link";
+import { BriefcaseBusiness, UserRoundCog } from "lucide-react";
+import { Button } from "./ui/button";
+import { authClient } from "@/lib/auth-client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { toast } from "sonner";
+import { redirect } from "next/navigation";
+
+export function Navbar() {
+  async function handleSignOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onError: (ctx) => {
+          toast.error(ctx.error.message);
+        },
+        onSuccess: () => {
+          redirect("/auth/login");
+        },
+      },
+    });
+  }
+
+  return (
+    <nav className="flex items-center justify-between p-4 border border-b">
+      <Link href="/">
+        <BriefcaseBusiness />
+      </Link>
+
+      <div className="flex items-center gap-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button size="icon" variant="outline">
+              <UserRoundCog />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem asChild>
+              <Link href="/profile">Profile</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuItem onClick={handleSignOut} variant="destructive">
+              Log Out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  );
+}
